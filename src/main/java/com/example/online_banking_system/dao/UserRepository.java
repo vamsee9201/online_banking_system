06400483845c7524d4ba5f92d5bc15beb0a5c270
Vamsee Krishna <vamseekrishna9201@gmail.com>
2024-09-07 22:18:49 -0500

    added user service impl

--- src/main/java/com/example/online_banking_system/dao/UserRepository.java
+++ src/main/java/com/example/online_banking_system/dao/UserRepository.java
@@ -1,9 +1,14 @@
 package com.example.online_banking_system.dao;
 
 import java.util.List;
-import com.example.online_banking_system.entity.User;
+import java.util.Optional;
 
-public interface UserRepository {
+import com.example.online_banking_system.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+
 
     public List<User> findAllByOrderByLastNameAsc();
 }
--- src/main/java/com/example/online_banking_system/service/UserServiceImpl.java
+++ src/main/java/com/example/online_banking_system/service/UserServiceImpl.java
@@ -1,4 +1,49 @@
 package com.example.online_banking_system.service;
 
-public class UserServiceImpl {
+import com.example.online_banking_system.dao.UserRepository;
+import com.example.online_banking_system.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    public UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository theUserRepository) {
+        userRepository = theUserRepository;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAllByOrderByLastNameAsc();
+    }
+
+    @Override
+    public User findById(int theId) {
+        Optional<User> result = userRepository.findById(theId);
+        User theUser = null;
+
+        if(result.isPresent()) {
+            theUser = result.get();
+        }
+        else {
+            throw new RuntimeException("did not find item id" + theId);
+        }
+        return theUser;
+    }
+
+    @Override
+    public void save(User theUser) {
+        userRepository.save(theUser);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        userRepository.deleteById(theId);
+    }
 }

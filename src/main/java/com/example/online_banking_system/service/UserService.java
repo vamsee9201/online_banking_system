c2a93f31e7a6e9b73eb8b94355b319ff87149a3d
Vamsee Krishna <vamseekrishna9201@gmail.com>
2024-09-07 22:07:50 -0500

    finished user service

--- src/main/java/com/example/online_banking_system/service/UserService.java
+++ src/main/java/com/example/online_banking_system/service/UserService.java
@@ -1,4 +1,16 @@
 package com.example.online_banking_system.service;
 
+import com.example.online_banking_system.entity.User;
+import java.util.List;
+
+
 public interface UserService {
+
+    List<User> findAll();
+
+    User findById(int theId);
+
+    void save(User theUser);
+
+    void deleteById(int theId);
 }

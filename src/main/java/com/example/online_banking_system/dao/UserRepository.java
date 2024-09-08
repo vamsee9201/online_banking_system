f933eab986cf0ed5b8bcef9b8f18b69b68855338
Vamsee Krishna <vamseekrishna9201@gmail.com>
2024-09-07 22:03:03 -0500

    finished dao

--- src/main/java/com/example/online_banking_system/dao/UserRepository.java
+++ src/main/java/com/example/online_banking_system/dao/UserRepository.java
@@ -1,4 +1,9 @@
 package com.example.online_banking_system.dao;
 
+import java.util.List;
+import com.example.online_banking_system.entity.User;
+
 public interface UserRepository {
+
+    public List<User> findAllByOrderByLastNameAsc();
 }

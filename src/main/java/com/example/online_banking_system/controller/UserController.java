51bb3428598527ba5478704a8c8c020380fbf7a2
Vamsee Krishna <vamseekrishna9201@gmail.com>
2024-09-07 22:27:24 -0500

    finished controller

--- src/main/java/com/example/online_banking_system/controller/UserController.java
+++ src/main/java/com/example/online_banking_system/controller/UserController.java
@@ -1,4 +1,30 @@
 package com.example.online_banking_system.controller;
 
+import com.example.online_banking_system.entity.User;
+import com.example.online_banking_system.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/users")
 public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService theUserService) {userService = theUserService;}
+
+    @GetMapping("/list")
+    public String ListUsers(Model theModel) {
+        List<User> theUsers = userService.findAll();
+        theModel.addAttribute("users",theUsers);
+
+
+
+        return "users";
+    }
+
 }

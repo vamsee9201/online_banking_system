c19ec03e7c6bacf9524be4c2ebebb4cece13cfb4
Vamsee Krishna <vamseekrishna9201@gmail.com>
2024-09-07 19:58:13 -0500

    first commit

---
+++ src/test/java/com/example/online_banking_system/OnlineBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_banking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
